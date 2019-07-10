daf9333ab8b46becee73bc553a079a73dfc5bdfb
BL-CoderOvertime <brandonlively@live.com>
2019-07-10 15:36:08 -0400

    Initial Commit
    Setup Basic Android App

---
+++ app/src/main/java/com/labs14/roamly/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.labs14.roamly
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
