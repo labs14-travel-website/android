3bd92dcaffe80546c727262b8f4c57627d2bd18d
BL-CoderOvertime <brandonlively@live.com>
2019-07-23 16:05:15 -0400

    Setup Trip SQL Table (Untested)

---
+++ app/src/main/java/app/labs14/roamly/localStorage/TripDbHelper.kt
@@ -0,0 +1,42 @@
+package app.labs14.roamly.localStorage
+
+import android.content.Context
+import android.database.DatabaseErrorHandler
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class TripDbHelper(context: Context) : SQLiteOpenHelper(context,
+    DATABASE_NAME, null,
+    DATABASE_VERSION
+) {
+
+    constructor(context: Context, name: String, factory: SQLiteDatabase.CursorFactory, version: Int) : this(context) {}
+
+    constructor(
+        context: Context,
+        name: String,
+        factory: SQLiteDatabase.CursorFactory,
+        version: Int,
+        errorHandler: DatabaseErrorHandler
+    ) : this(context)
+
+    constructor(context: Context, name: String, version: Int, openParams: SQLiteDatabase.OpenParams) : this(context) {}
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(DbContract.TripEntry.SQL_CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(DbContract.TripEntry.SQL_DELETE_TABLE)
+        onCreate(db)
+    }
+
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "roamly_local.db"
+    }
+}
