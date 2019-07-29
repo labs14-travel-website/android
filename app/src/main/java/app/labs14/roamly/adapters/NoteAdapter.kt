3a835fad2276b1c29db2f3d30eb5b78679009766
Shoonsk8 <shoon@att.net>
2019-07-29 19:26:02 -0400

    recycler view with expandable fixed

--- app/src/main/java/app/labs14/roamly/adapters/NoteAdapter.kt
+++ app/src/main/java/app/labs14/roamly/adapters/NoteAdapter.kt
@@ -42,6 +42,9 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DIFF_CALLBACK) {
     override fun onBindViewHolder(holder: NoteHolder, position: Int) {
         val currentNote: Note = getItem(position)
 
+
+
+
         holder.textViewTitle.text = currentNote.title
         holder.textViewPriority.text = currentNote.priority.toString()
         holder.textViewDescription.text = currentNote.description
@@ -56,48 +59,48 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DIFF_CALLBACK) {
         internal var adapter: ExpandableListAdapter? = null
         internal var titleList: List<String> ? = null
         val data: HashMap<String, List<String>>
-            get() {
-                val listData = HashMap<String, List<String>>()
+        get() {
+            val listData = HashMap<String, List<String>>()
 
-                val redmiMobiles = ArrayList<String>()
-                redmiMobiles.add("Redmi Y2")
-                redmiMobiles.add("Redmi S2")
-                redmiMobiles.add("Redmi Note 5 Pro")
-                redmiMobiles.add("Redmi Note 5")
-                redmiMobiles.add("Redmi 5 Plus")
-                redmiMobiles.add("Redmi Y1")
-                redmiMobiles.add("Redmi 3S Plus")
+            val redmiMobiles = ArrayList<String>()
+            redmiMobiles.add("Redmi Y2")
+            redmiMobiles.add("Redmi S2")
+            redmiMobiles.add("Redmi Note 5 Pro")
+            redmiMobiles.add("Redmi Note 5")
+            redmiMobiles.add("Redmi 5 Plus")
+            redmiMobiles.add("Redmi Y1")
+            redmiMobiles.add("Redmi 3S Plus")
 
-                val micromaxMobiles = ArrayList<String>()
-                micromaxMobiles.add("Micromax Bharat Go")
-                micromaxMobiles.add("Micromax Bharat 5 Pro")
-                micromaxMobiles.add("Micromax Bharat 5")
-                micromaxMobiles.add("Micromax Canvas 1")
-                micromaxMobiles.add("Micromax Dual 5")
+            val micromaxMobiles = ArrayList<String>()
+            micromaxMobiles.add("Micromax Bharat Go")
+            micromaxMobiles.add("Micromax Bharat 5 Pro")
+            micromaxMobiles.add("Micromax Bharat 5")
+            micromaxMobiles.add("Micromax Canvas 1")
+            micromaxMobiles.add("Micromax Dual 5")
 
-                val appleMobiles = ArrayList<String>()
-                appleMobiles.add("iPhone 8")
-                appleMobiles.add("iPhone 8 Plus")
-                appleMobiles.add("iPhone X")
-                appleMobiles.add("iPhone 7 Plus")
-                appleMobiles.add("iPhone 7")
-                appleMobiles.add("iPhone 6 Plus")
+            val appleMobiles = ArrayList<String>()
+            appleMobiles.add("iPhone 8")
+            appleMobiles.add("iPhone 8 Plus")
+            appleMobiles.add("iPhone X")
+            appleMobiles.add("iPhone 7 Plus")
+            appleMobiles.add("iPhone 7")
+            appleMobiles.add("iPhone 6 Plus")
 
-                val samsungMobiles = ArrayList<String>()
-                samsungMobiles.add("Samsung Galaxy S9+")
-                samsungMobiles.add("Samsung Galaxy Note 7")
-                samsungMobiles.add("Samsung Galaxy Note 5 Dual")
-                samsungMobiles.add("Samsung Galaxy S8")
-                samsungMobiles.add("Samsung Galaxy A8")
-                samsungMobiles.add("Samsung Galaxy Note 4")
+            val samsungMobiles = ArrayList<String>()
+            samsungMobiles.add("Samsung Galaxy S9+")
+            samsungMobiles.add("Samsung Galaxy Note 7")
+            samsungMobiles.add("Samsung Galaxy Note 5 Dual")
+            samsungMobiles.add("Samsung Galaxy S8")
+            samsungMobiles.add("Samsung Galaxy A8")
+            samsungMobiles.add("Samsung Galaxy Note 4")
 
-                listData["Redmi"] = redmiMobiles
-                listData["Micromax"] = micromaxMobiles
-                listData["Apple"] = appleMobiles
-                listData["Samsung"] = samsungMobiles
+            listData["Redmi"] = redmiMobiles
+            listData["Micromax"] = micromaxMobiles
+            listData["Apple"] = appleMobiles
+            listData["Samsung"] = samsungMobiles
 
-                return listData
-            }
+            return listData
+        }
         init {
             itemView.setOnClickListener {
                 val position = adapterPosition
