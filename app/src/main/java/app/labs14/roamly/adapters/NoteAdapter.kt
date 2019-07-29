c608b38c4d4b1cf46870bff5368f6928cbf0a043
Shoonsk8 <shoon@att.net>
2019-07-29 19:11:52 -0400

    recycler view with expandable

--- app/src/main/java/app/labs14/roamly/adapters/NoteAdapter.kt
+++ app/src/main/java/app/labs14/roamly/adapters/NoteAdapter.kt
@@ -3,7 +3,10 @@ package app.labs14.roamly.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +17,7 @@ import kotlinx.android.synthetic.main.note_item.view.*
 
 //shoon 2019/07/26
 class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DIFF_CALLBACK) {
+
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Note>() {
@@ -48,6 +52,52 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DIFF_CALLBACK) {
     }
 
     inner class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var expandableListView: ExpandableListView = itemView.findViewById(R.id.expandableListView2)
+        internal var adapter: ExpandableListAdapter? = null
+        internal var titleList: List<String> ? = null
+        val data: HashMap<String, List<String>>
+            get() {
+                val listData = HashMap<String, List<String>>()
+
+                val redmiMobiles = ArrayList<String>()
+                redmiMobiles.add("Redmi Y2")
+                redmiMobiles.add("Redmi S2")
+                redmiMobiles.add("Redmi Note 5 Pro")
+                redmiMobiles.add("Redmi Note 5")
+                redmiMobiles.add("Redmi 5 Plus")
+                redmiMobiles.add("Redmi Y1")
+                redmiMobiles.add("Redmi 3S Plus")
+
+                val micromaxMobiles = ArrayList<String>()
+                micromaxMobiles.add("Micromax Bharat Go")
+                micromaxMobiles.add("Micromax Bharat 5 Pro")
+                micromaxMobiles.add("Micromax Bharat 5")
+                micromaxMobiles.add("Micromax Canvas 1")
+                micromaxMobiles.add("Micromax Dual 5")
+
+                val appleMobiles = ArrayList<String>()
+                appleMobiles.add("iPhone 8")
+                appleMobiles.add("iPhone 8 Plus")
+                appleMobiles.add("iPhone X")
+                appleMobiles.add("iPhone 7 Plus")
+                appleMobiles.add("iPhone 7")
+                appleMobiles.add("iPhone 6 Plus")
+
+                val samsungMobiles = ArrayList<String>()
+                samsungMobiles.add("Samsung Galaxy S9+")
+                samsungMobiles.add("Samsung Galaxy Note 7")
+                samsungMobiles.add("Samsung Galaxy Note 5 Dual")
+                samsungMobiles.add("Samsung Galaxy S8")
+                samsungMobiles.add("Samsung Galaxy A8")
+                samsungMobiles.add("Samsung Galaxy Note 4")
+
+                listData["Redmi"] = redmiMobiles
+                listData["Micromax"] = micromaxMobiles
+                listData["Apple"] = appleMobiles
+                listData["Samsung"] = samsungMobiles
+
+                return listData
+            }
         init {
             itemView.setOnClickListener {
                 val position = adapterPosition
@@ -55,11 +105,38 @@ class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DIFF_CALLBACK) {
                     listener?.onItemClick(getItem(position))
                 }
             }
+            if (expandableListView != null) {
+                val listData = data
+                titleList = ArrayList(listData.keys)
+                adapter = CustomExpandableListAdapter(itemView.context, titleList as ArrayList<String>, listData)
+                expandableListView!!.setAdapter(adapter)
+
+                expandableListView!!.setOnGroupExpandListener {
+
+                }
+
+                expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+
+                }
+
+                expandableListView!!.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+
+                    false
+                }
+            }
         }
 
         var textViewTitle: TextView = itemView.text_view_title
         var textViewPriority: TextView = itemView.text_view_priority
         var textViewDescription: TextView = itemView.text_view_description
+
+
+
+
+
+
+
+
     }
 
     interface OnItemClickListener {
--- app/src/main/java/app/labs14/roamly/views/LoginGoogleActivity.kt
+++ app/src/main/java/app/labs14/roamly/views/LoginGoogleActivity.kt
@@ -7,9 +7,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 import android.content.Intent
 import android.view.View
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import app.labs14.roamly.data.NetworkAdapter
 import app.labs14.roamly.R
+import app.labs14.roamly.adapters.CustomExpandableListAdapter
 import app.labs14.roamly.localStorage.DbHelper
 import app.labs14.roamly.localStorage.SqlDao
 import com.google.android.gms.common.SignInButton
@@ -22,6 +26,54 @@ private var dbDao: SqlDao? = null
 
 class LoginGoogleActivity : AppCompatActivity() {
 
+
+    internal var expandableListView: ExpandableListView? = null
+    internal var adapter: ExpandableListAdapter? = null
+    internal var titleList: List<String> ? = null
+
+    val data: HashMap<String, List<String>>
+        get() {
+            val listData = HashMap<String, List<String>>()
+
+            val redmiMobiles = ArrayList<String>()
+            redmiMobiles.add("Redmi Y2")
+            redmiMobiles.add("Redmi S2")
+            redmiMobiles.add("Redmi Note 5 Pro")
+            redmiMobiles.add("Redmi Note 5")
+            redmiMobiles.add("Redmi 5 Plus")
+            redmiMobiles.add("Redmi Y1")
+            redmiMobiles.add("Redmi 3S Plus")
+
+            val micromaxMobiles = ArrayList<String>()
+            micromaxMobiles.add("Micromax Bharat Go")
+            micromaxMobiles.add("Micromax Bharat 5 Pro")
+            micromaxMobiles.add("Micromax Bharat 5")
+            micromaxMobiles.add("Micromax Canvas 1")
+            micromaxMobiles.add("Micromax Dual 5")
+
+            val appleMobiles = ArrayList<String>()
+            appleMobiles.add("iPhone 8")
+            appleMobiles.add("iPhone 8 Plus")
+            appleMobiles.add("iPhone X")
+            appleMobiles.add("iPhone 7 Plus")
+            appleMobiles.add("iPhone 7")
+            appleMobiles.add("iPhone 6 Plus")
+
+            val samsungMobiles = ArrayList<String>()
+            samsungMobiles.add("Samsung Galaxy S9+")
+            samsungMobiles.add("Samsung Galaxy Note 7")
+            samsungMobiles.add("Samsung Galaxy Note 5 Dual")
+            samsungMobiles.add("Samsung Galaxy S8")
+            samsungMobiles.add("Samsung Galaxy A8")
+            samsungMobiles.add("Samsung Galaxy Note 4")
+
+            listData["Redmi"] = redmiMobiles
+            listData["Micromax"] = micromaxMobiles
+            listData["Apple"] = appleMobiles
+            listData["Samsung"] = samsungMobiles
+
+            return listData
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_google)
@@ -31,6 +83,26 @@ class LoginGoogleActivity : AppCompatActivity() {
         googleLoginInit()
         btn_offline.setOnClickListener { offlineSignOn()}
         btn_note.setOnClickListener{openNote()}     //shoon 2019/07/26
+        expandableListView = findViewById(R.id.expandableListView)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+
+            expandableListView!!.setOnGroupExpandListener {
+                    groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Expanded.", Toast.LENGTH_SHORT).show()
+            }
+
+            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.", Toast.LENGTH_SHORT).show()
+            }
+
+            expandableListView!!.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+                Toast.makeText(applicationContext, "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(titleList as ArrayList<String>)[groupPosition]]!!.get(childPosition), Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
     }
 
     private fun debugMessages(){tv_debug.visibility = View.VISIBLE}
